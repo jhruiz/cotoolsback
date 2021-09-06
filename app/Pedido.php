64a66a6f8a27f3d0d3e6570420b331f714bcbfda
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2021-09-05 23:16:49 -0500

    ajuste en consulta

--- app/Pedido.php
+++ app/Pedido.php
@@ -15,6 +15,7 @@ class Pedido extends Model
                               'usuarios.email', 'estadospedidos.descripcion')
                 ->join('usuarios', 'usuarios.id', '=', 'pedidos.usuario_id')
                 ->join('estadospedidos', 'estadospedidos.id', '=', 'pedidos.estadospedido_id')
+                ->where('pedidos.nro_pdweb', '<>', null)                
                 ->groupBy(  'pedidos.id', 'pedidos.nro_pdweb', 'pedidos.estadopago', 'pedidos.created',
                             'usuarios.nombre', 'usuarios.identificacion', 'usuarios.email', 'estadospedidos.descripcion')  
                 ->get();
